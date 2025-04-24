e8710af87dd4d4de6e4f455318cba28cdbefe4e9
Oriolapeter <115485533+Oriolapeter@users.noreply.github.com>
2025-04-23 20:00:00 -0400

    url fix

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ToastToolkit",
-            url: "https://github.com/Peteroriola/ToastToolkit-SPM/releases/tag/1.0.0/ToastToolkit.xcframework.zip",
+            url: "https://github.com/peteroriola/ToastToolkit-SPM/releases/tag/1.0.0/ToastToolkit.xcframework.zip",
             checksum: "7bdf7e1be6eb4ba9c31d86404486fd35baf397803e04a91e90ff23e8636a0af3"
         )
     ]
